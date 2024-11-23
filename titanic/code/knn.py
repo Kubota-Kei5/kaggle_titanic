b35cf71fe5b3ddf6616e5347089ddcbd797eb909
k5 <keigo1003560@gmail.com>
2024-11-23 08:23:31 +0000

    knnの特長量いじってみた。 ①get_dummiesでone-hotエンコーディング ②Fareの標準化

--- titanic/code/knn.py
+++ titanic/code/knn.py
@@ -38,4 +38,72 @@ submit['Survived']=knn.predict(x_for_submit)
 submit
 # %%
 submit.to_csv('../submission/submit01_knn.csv', index=False)
+
+
+
+
+# %%
+#Feature Improvement and Feature Selection
+# %%
+#Feature addition
+# %%
+x=train[['Pclass','SibSp','Parch','Sex']]
+y=train[['Survived']]
+# %%
+x=pd.get_dummies(x, dtype=int, columns=['Pclass', 'Sex'])
+x
+# %%
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+
+knn=KNeighborsClassifier()
+knn.fit(x_train, y_train)
+
+knn.score(x_test, y_test)
+# %%
+#Feature addition and removing null
+# %%
+x=train[['Pclass','SibSp','Parch','Sex','Fare']]
+y=train[['Survived']]
+# %%
+x=pd.get_dummies(x, dtype=int, columns=['Pclass', 'Sex'])
+x
+# %%
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+
+knn=KNeighborsClassifier()
+knn.fit(x_train, y_train)
+
+knn.score(x_test, y_test)
+# %%
+#Standardization
+# %%
+from sklearn.preprocessing import StandardScaler
+
+scaler=StandardScaler()
+scaler.fit(x_train)
+
+x_train_scaled=scaler.transform(x_train)
+x_test_scaled=scaler.transform(x_test)
+
+knn=KNeighborsClassifier()
+knn.fit(x_train_scaled, y_train)
+
+knn.score(x_test_scaled, y_test)
+# %%
+x_for_submit = test[['Pclass', 'SibSp', 'Parch', 'Sex', 'Fare']]
+submit = test[['PassengerId']]
+
+x_for_submit=pd.get_dummies(x_for_submit, columns=['Pclass', 'Sex'])
+
+x_for_submit['Fare']=x_for_submit['Fare'].fillna(x_for_submit['Fare'].mean())
+
+scaler=StandardScaler()
+scaler.fit(x_for_submit)
+
+x_for_submit_scaled=scaler.transform(x_for_submit)
+
+submit['Survived']=knn.predict(x_for_submit_scaled)
+submit
+# %%
+submit.to_csv('../submission/submit02_knn.csv', index=False)
 # %%
